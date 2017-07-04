8c766c898ffe9d69c57452a685326929805bda51
Thomas Theunen <thomas.theunen@fenego.be>
2017-07-04 07:24:20 +0200

    Refactoring
    Move all view logic to main.js where it belongs. Split up authenticating and fetching data.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ requirejs(["./synology", "vendor/jquery-2.1.4.min", "helper/util", ], function(S
   $(document).ready(function() {
     doTranslation();
     reloadMain();
+    initJqueryEvents();
+  });
 
+  function initJqueryEvents() {
     $('#login-submit').on('click', function(e) {
       var username = $('#login-form').find('#username').val();
       var password = $('#login-form').find('#password').val();
@@ -42,7 +45,7 @@ requirejs(["./synology", "vendor/jquery-2.1.4.min", "helper/util", ], function(S
         $('#login-submit').trigger('click');
       }
     });
-  });
+  }
 
   function reloadMain() {
     $('.dsm-info').hide();
@@ -56,7 +59,7 @@ requirejs(["./synology", "vendor/jquery-2.1.4.min", "helper/util", ], function(S
 
         $('.logged-in-as').text(chrome.i18n.getMessage("welcome") + result.username);
 
-        Synology.fetchStatus(result.username, result.password, result.server, result.ssl);
+        loginAndLoadDSMData(result.username, result.password, result.server, result.ssl);
       } else {
         $('.logged-in-panel').hide();
         $('.login-panel').show();
@@ -69,6 +72,72 @@ requirejs(["./synology", "vendor/jquery-2.1.4.min", "helper/util", ], function(S
           $('#login-form').find('#ssl').prop('checked', false);
         }
       }
+    });
+  }
+
+  function loginAndLoadDSMData(username, password, server, ssl) {
+    Synology.authenticate(username, password, server, ssl, function(baseUrl) {
+      loadDSMData(baseUrl);
+    }, function(error) {
+      $('.loader').hide();
+      showError(error);
+    });
+  }
+
+  function loadDSMData(baseUrl) {
+    Synology.startFetchDSMData(baseUrl, function(result) {
+      var infoResult = result.info;
+      var entryResult = result.entry;
+      var baseUrl = result.baseUrl;
+
+      var uptime = infoResult.optime;
+      var splitUptime = uptime.split(':');
+
+
+      $('.dsmmodel').text(infoResult.disks[0].container.str);
+      $('.dsmtemperature').text(infoResult.disks[0].temp + ' (' + ((infoResult.temperature_warning) ? "NOK" : "OK") + ")");
+      $('.dsmuptime').text(splitUptime[0] + ' ' + chrome.i18n.getMessage("hours") + ' ' + splitUptime[1] + ' ' + chrome.i18n.getMessage("minutes") + ' ' + splitUptime[2] + ' ' + chrome.i18n.getMessage("seconds"));
+
+      var diskUsage = infoResult.vol_info[0].used_size / infoResult.vol_info[0].total_size;
+      var used_size_mb = Math.trunc(infoResult.vol_info[0].used_size / 1024 / 1024 / 1024);
+      var total_size_mb = Math.trunc(infoResult.vol_info[0].total_size / 1024 / 1024 / 1024);
+      $('.dsmdiskspaceused').text(used_size_mb + " / " + total_size_mb + " MB");
+      $('.dsmdiskspace').width(Math.trunc(diskUsage * 100) + '%');
+
+      if (diskUsage > 0.7) {
+        $('.dsmdiskspace').addClass('progress-bar-warning');
+      } else if (diskUsage > 0.9) {
+        $('.dsmdiskspace').addClass('progress-bar-danger');
+      }
+
+      var cpuPercentage = entryResult.data.result[1].data.cpu.other_load + entryResult.data.result[1].data.cpu.system_load + entryResult.data.result[1].data.cpu.user_load;
+      $('.dsmcpu').text(cpuPercentage + " %");
+      $('.dsmcpupercentage').width(cpuPercentage + "%");
+
+      if (cpuPercentage > 70) {
+        $('.dsmcpupercentage').addClass('progress-bar-warning');
+      } else if (cpuPercentage > 90) {
+        $('.dsmcpupercentage').addClass('progress-bar-danger');
+      }
+
+      var memoryUsage = entryResult.data.result[1].data.memory.real_usage;
+
+      $('.dsmmemory').text(memoryUsage + " %");
+      $('.dsmmemorypercentage').width(memoryUsage + "%");
+
+      if (memoryUsage > 70) {
+        $('.dsmmemorypercentage').addClass('progress-bar-warning');
+      } else if (cpuPercentage > 90) {
+        $('.dsmmemorypercentage').addClass('progress-bar-danger');
+      }
+
+      $('.dsm-info').show();
+      $('.loader').hide();
+      _gaq.push(['_trackPageview', '/refresh.html']);
+
+      setTimeout(loadDSMData, 5000, baseUrl);
+    }, function(error) {
+      showError(error);
     });
   }
 
--- js/synology.js
+++ js/synology.js
@@ -1,87 +1,45 @@
 define(["vendor/quickconnectid.min"], function() {
-    var Synology = {
-        fetchStatus: function(username, password, server, ssl) {
-            getBaseUrl(server, ssl, function(baseUrl) {
-                    $.get(baseUrl + "/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=" + encodeURIComponent(username) + "&passwd=" + encodeURIComponent(password) + "&session=DownloadStation&format=sid", function(data) {
+  var Synology = {
+    authenticate: function(username, password, server, ssl, success, error) {
+      getBaseUrl(server, ssl, function(baseUrl) {
+          $.get(baseUrl + "/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=" + encodeURIComponent(username) + "&passwd=" + encodeURIComponent(password) + "&session=DownloadStation&format=sid", function(data) {
 
-                        var loginResult = JSON.parse(data);
-                        if (loginResult.success) {
-                            Synology.startFetchDSMData(baseUrl);
-                        } else {
-                            showError(chrome.i18n.getMessage("loginError"));
-                            $('.loader').hide();
-                        }
-                    }).error(function() {
-                        showError(chrome.i18n.getMessage("loginError"));
-                        $('.loader').hide();
-                    });
-                },
-                function(error) {
-                    showError(error);
-                    $('.loader').hide();
-                });
+            var loginResult = JSON.parse(data);
+            if (loginResult.success) {
+              success(baseUrl)
+            } else {
+              error(chrome.i18n.getMessage("loginError"));
+
+            }
+          }).error(function() {
+            error(chrome.i18n.getMessage("loginError"));
+
+          });
         },
-        startFetchDSMData: function(baseUrl) {
-            $.get(baseUrl + "/webman/modules/SystemInfoApp/SystemInfo.cgi?query=systemHealth", function(info) {
-                var infoResult = JSON.parse(info);
-                $.get(baseUrl + "/webapi/entry.cgi?stop_when_error=false&compound=%5B%7B%22api%22%3A%22SYNO.Entry.Request%22%2C%22method%22%3A%22request%22%2C%22version%22%3A1%2C%22compound%22%3A%5B%7B%22api%22%3A%22SYNO.Core.AppNotify%22%2C%22method%22%3A%22get%22%2C%22version%22%3A1%7D%5D%7D%2C%7B%22api%22%3A%22SYNO.Core.System.Utilization%22%2C%22method%22%3A%22get%22%2C%22version%22%3A1%2C%22type%22%3A%22current%22%7D%5D&api=SYNO.Entry.Request&method=request&version=1", function(entry) {
-                    var entryResult = entry;
-                    if (infoResult.disks) {
-                        var uptime = infoResult.optime;
-                        var splitUptime = uptime.split(':');
+        function(error) {
+          error(error);
+        });
+    },
+    startFetchDSMData: function(baseUrl, success, error) {
+      $.get(baseUrl + "/webman/modules/SystemInfoApp/SystemInfo.cgi?query=systemHealth", function(info) {
+        var infoResult = JSON.parse(info);
+        $.get(baseUrl + "/webapi/entry.cgi?stop_when_error=false&compound=%5B%7B%22api%22%3A%22SYNO.Entry.Request%22%2C%22method%22%3A%22request%22%2C%22version%22%3A1%2C%22compound%22%3A%5B%7B%22api%22%3A%22SYNO.Core.AppNotify%22%2C%22method%22%3A%22get%22%2C%22version%22%3A1%7D%5D%7D%2C%7B%22api%22%3A%22SYNO.Core.System.Utilization%22%2C%22method%22%3A%22get%22%2C%22version%22%3A1%2C%22type%22%3A%22current%22%7D%5D&api=SYNO.Entry.Request&method=request&version=1", function(entry) {
+          var entryResult = entry;
+          if (infoResult.disks) {
+            success({
+              baseUrl: baseUrl,
+              info: infoResult,
+              entry: entryResult
+            })
+          } else {
+            error("Error fetching DSM status!");
+          }
+        });
+
+      });
 
 
-                        $('.dsmmodel').text(infoResult.disks[0].container.str);
-                        $('.dsmtemperature').text(infoResult.disks[0].temp + ' (' + ((infoResult.temperature_warning) ? "NOK" : "OK") + ")");
-                        $('.dsmuptime').text(splitUptime[0] + ' ' + chrome.i18n.getMessage("hours") + ' ' + splitUptime[1] + ' ' + chrome.i18n.getMessage("minutes") + ' ' + splitUptime[2] + ' ' + chrome.i18n.getMessage("seconds"));
-
-                        var diskUsage = infoResult.vol_info[0].used_size / infoResult.vol_info[0].total_size;
-                        var used_size_mb = Math.trunc(infoResult.vol_info[0].used_size / 1024 / 1024 / 1024);
-                        var total_size_mb = Math.trunc(infoResult.vol_info[0].total_size / 1024 / 1024 / 1024);
-                        $('.dsmdiskspaceused').text(used_size_mb + " / " + total_size_mb + " MB");
-                        $('.dsmdiskspace').width(Math.trunc(diskUsage * 100) + '%');
-
-                        if (diskUsage > 0.7) {
-                            $('.dsmdiskspace').addClass('progress-bar-warning');
-                        } else if (diskUsage > 0.9) {
-                            $('.dsmdiskspace').addClass('progress-bar-danger');
-                        }
-
-                        var cpuPercentage = entryResult.data.result[1].data.cpu.other_load + entryResult.data.result[1].data.cpu.system_load + entryResult.data.result[1].data.cpu.user_load;
-                        $('.dsmcpu').text(cpuPercentage + " %");
-                        $('.dsmcpupercentage').width(cpuPercentage + "%");
-
-                        if (cpuPercentage > 70) {
-                            $('.dsmcpupercentage').addClass('progress-bar-warning');
-                        } else if (cpuPercentage > 90) {
-                            $('.dsmcpupercentage').addClass('progress-bar-danger');
-                        }
-
-                        var memoryUsage = entryResult.data.result[1].data.memory.real_usage;
-
-                        $('.dsmmemory').text(memoryUsage + " %");
-                        $('.dsmmemorypercentage').width(memoryUsage + "%");
-
-                        if (memoryUsage > 70) {
-                            $('.dsmmemorypercentage').addClass('progress-bar-warning');
-                        } else if (cpuPercentage > 90) {
-                            $('.dsmmemorypercentage').addClass('progress-bar-danger');
-                        }
-
-                        $('.dsm-info').show();
-                        $('.loader').hide();
-                        _gaq.push(['_trackPageview', '/refresh.html']);
-
-                        setTimeout(Synology.startFetchDSMData, 5000, baseUrl);
-                    } else {
-                        showError("Error fetching DSM status!");
-                    }
-                });
-
-            });
-
-
-        }
-    };
-    return Synology;
+    }
+  };
+  return Synology;
 });
